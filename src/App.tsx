599cc925129a99ae2e5496a278de91e0747b1c6b
Dan4ik7 <104564622+Dan4ik7@users.noreply.github.com>
2023-02-14 13:39:23 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+        <p>HELLO GUYS!</p>
+        <p> TODAY I'M GONNA TEACH YOU HOW TO INSTALL REACT.</p>
+        <p> MY NAME IS DANIEL MIRON</p>
+        <p>FROM CR-211</p>
+        </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
